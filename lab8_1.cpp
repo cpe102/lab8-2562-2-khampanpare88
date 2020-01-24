73b12a82967e9572f2abca9388e29c6040f272aa
lab302-8 <lab302-8@labeng.local>
2020-01-24 16:43:07 +0700

    lab8_1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main(){	
-
+	int initial;
+	float rate,pay;
+	cout<<"Enter initial loan:";
+	cin>>initial;
+	cout<<"Enter interest rate per year (%):";
+	cin>>rate;
+	cout<<"Enter amount you can pay per year:";
+	cin>>pay;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,14 +24,46 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	int E=1;
+	float P,I,T,N;
+		
+		P=initial;
+		I=P*rate/100.00;
+		T=P+I;
+		N=T-pay;
+		if(N<pay){pay=T;
+		N=pay-T;}
+
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << E; 
+		cout << setw(13) << left << P;
+		cout << setw(13) << left << I;
+		cout << setw(13) << left << T;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << N;
+		cout << "\n";
+		
+	
+			while(N>0){
+			P=N;
+			I=P*rate/100;
+			T=P+I;
+			
+			E++;
+			if(N<pay){pay=T;}
+			N=T-pay;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << E; 
+			cout << setw(13) << left << P;
+			cout << setw(13) << left << I;
+			cout << setw(13) << left << T;
+			cout << setw(13) << left << pay;
+			cout << setw(13) << left << N;
+			cout << "\n";	
+			
+		}
+	
+
 	return 0;
 }
